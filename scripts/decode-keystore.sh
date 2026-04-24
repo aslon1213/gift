35e8d3420578174ee4f48c58bf75cec7b92f181f
Aslon Khamidov <hamidovaslon1@gmail.com>
2026-04-24 10:05:54 +0500

    build kts change

--- scripts/decode-keystore.sh
+++ scripts/decode-keystore.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-echo $KEYSTORE_BASE64 | base64 --decode > client/gift/src-tauri/gen/android/app/gift.keystore
-export KEYSTORE_PATH="$PWD/client/gift/src-tauri/gen/android/app/gift.keystore"
+set -e
+
+KEYSTORE_OUT="client/gift/src-tauri/gen/android/app/gift.keystore"
+echo "$KEYSTORE_BASE64" | base64 --decode > "$KEYSTORE_OUT"
+echo "Keystore decoded to $KEYSTORE_OUT"
+
+# Export for subsequent hooks
+export KEYSTORE_PATH="$(pwd)/$KEYSTORE_OUT"
